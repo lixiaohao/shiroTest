2fb32fee6b167de2f386d4e506f126b463866471
xiaohao.li <xiaohao.li@chinaredstar.com>
2018-05-09 11:52:14 +0800

    shiro chapter1

--- shiroTest-chapter4/src/main/java/com/lixiaohao/test/springshiro/controller/ArticleController.java
+++ shiroTest-chapter4/src/main/java/com/lixiaohao/test/springshiro/controller/ArticleController.java
@@ -1,19 +1,23 @@
 package com.lixiaohao.test.springshiro.controller;
 
 import com.lixiaohao.test.springshiro.model.Article;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
-@RequestMapping("/article")
+@RequestMapping("article")
 public class ArticleController {
 
 
     @RequestMapping("find")
-    public List<Article> findArticles(){
+    @ResponseBody
+    public ResponseEntity<List<Article>> findArticles(){
 
         Article article = new Article();
         article.setAuthor("张三");
@@ -22,7 +26,7 @@ public class ArticleController {
         List<Article> articles = new ArrayList<Article>();
         articles.add(article);
 
-        return articles;
+        return new ResponseEntity<List<Article>>(articles, HttpStatus.OK);
 
     }
 
